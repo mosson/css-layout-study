76778b9a969b3b5984f2ed5fe4fac41df7d876eb
a-munakata <a.munakata@4digit.jp>
2013-05-17 21:34:35 +0900

    Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,70 @@
-module.exports = function (grunt) { 
-  grunt.initConfig({    
-    sass: {
-      dist: {
-        files: {
-          'lesson1/css/index.css': 'src/scss/lesson1/style.scss'
-        },
-        options: {
-          style: 'expanded'
-        }
-      }
-    },    
-    connect: {  
-      server1: {
-        options: {
-          // localhost:9001 のサーバーを立てる          
-          port: 9001,
-          base: 'lesson1/'
-        }
-      },
-      server2: {
-        options: {
-          // localhost:9002 のサーバーを立てる
-          port: 9002,
-          base: 'lesson2/'
-        }
-      }      
-    },
-    livereload: {
-      // localhost:35729 のサーバーを立てる
-      port: 35729
-    },
-    regarde: {
-      livereload: {
-        files: ['lesson1/*.html'],
-        tasks: ['livereload']
-      },
-      sass: {
-        files: ['src/scss/lesson1/style.scss'],
-        tasks: ['sass']
-      }
-    }
-  });
+
+module.exports = function (grunt) {   
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-contrib-livereload');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  
-  grunt.registerTask('default', ['livereload-start', 'connect' ,'regarde']);
+
+  // レッスン数  
+  var LESSON_N = 1;
+
+  var sass_path = function(n) {
+  var result = {};
+  for ( var i = 1; i <= n; i ++ ) {
+    var index = i;
+    // resultというオブジェクトの中にkey,値を格納する
+    result["app/lesson" + (index) + "/css/index.css"] = ["src/scss/lesson" + (index) + "/index.scss"];
+  }
+  return result;
+  };
+
+  grunt.initConfig({  
+    sass: {
+      dist: {
+        files: sass_path(LESSON_N),
+        options: {
+          style: 'compressed'
+        }
+      }
+    },
+
+    connect: {        
+      dev: {　　// 開発用 connect        
+        options: {
+          port: 9001,
+          base: 'app/'
+        }
+      },      
+      production: {　　// 閲覧用 connect 
+        options: {
+          port: 9001,
+          base: 'app/',
+          keepalive: true
+        }
+      }    
+    },
+
+    livereload: {
+      // livereload用 localhost:35729 のサーバーを立てる
+      port: 35729
+    },
+
+    regarde: {
+      livereload: {
+        files: ['app/**/*.html'],
+        tasks: ['livereload']
+      },
+      sass: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+    
+
+  // 開発用: grunt
+  grunt.registerTask('default', ['livereload-start', 'connect:dev' ,'regarde']);    
+  // 閲覧用: grunt production
+  grunt.registerTask('production', ['sass', 'connect:production']);
 }
 
